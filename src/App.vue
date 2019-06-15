21df0a0c3e98a785f4f323b372155d786fe62976
Heraldo Yusron <heraldoyusrontris@gmail.com>
2019-06-15 23:56:27 +0700

    navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,60 @@
   <v-app>
   <div id="app">
     <v-toolbar id="toolbar">
-      <v-toolbar-title><img src="./assets/GS.png" width="200" alt=""></v-toolbar-title>
-      <router-link id="navbar-text" to="/"><v-toolbar-title>Home</v-toolbar-title></router-link>
+      <v-toolbar-title @click.stop="drawer = !drawer"><img src="./assets/GS.png" width="200" alt=""></v-toolbar-title>
+      <!-- <router-link id="navbar-text" to="/"><v-toolbar-title>Home</v-toolbar-title></router-link>
       <router-link id="navbar-text" to="/Search"><v-toolbar-title>Search</v-toolbar-title></router-link>
-      <router-link id="navbar-text" to="/login"><v-toolbar-title>Register/Login</v-toolbar-title></router-link>
+      <router-link id="navbar-text" to="/login"><v-toolbar-title>Register/Login</v-toolbar-title></router-link> -->
       <v-spacer></v-spacer>
     </v-toolbar>
     <router-view/>
   </div>
+
+  <v-navigation-drawer
+    v-model="drawer"
+    absolute
+    temporary
+    dark
+  >
+    <img src="./assets/GS.png" width="200" alt="">
+    <v-list>
+      <v-list-tile
+        v-for="item in items"
+        :key="item.title"
+        avatar
+        @click=""
+      >
+        <v-list-tile-content>
+          <router-link id="router-link" :to="item.link" ><v-list-tile-title class="navbar-text" v-text="item.title"></v-list-tile-title></router-link>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
   </v-app>
 </template>
 
+<script>
+  export default {
+    data(){
+      return {
+        drawer: null,
+        items: [
+          { title:'HOME', link:'/home'},
+          { title:'SEARCH', link:'/search'},
+          { title:'REGISTER / LOGIN', link:'/login'}
+        ],
+        mini: false,
+        right: null
+      }
+    }
+  }
+</script>
+
 <style lang="scss">
+#router-link{
+  text-decoration: none;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -21,16 +63,17 @@
   text-align: center;
   color: #2c3e50;
 }
-#navbar-text{
-  text-decoration: none;
+.navbar-text{
   color: #ffffff;
+  font-size: 16px;
   margin-right: 30px;
   border-bottom: 3px solid rgba(255, 255, 255, 0);
-  transition: 0.3s;
+  transition: 0.5;
 }
 
-#navbar-text:hover{
-  border-bottom: 3px solid #ffffff;
+.navbar-text:hover{
+  color: #ff3300;
+  border-bottom: 3px solid #ff3300;
 }
 
 #toolbar {
