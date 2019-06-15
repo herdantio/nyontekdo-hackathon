84161a1dae73774e77a0f04eff828dec507ac8d9
Heraldo Yusron <heraldoyusrontris@gmail.com>
2019-06-16 02:43:37 +0700

    detail with error

--- src/views/place/PlaceDetail.vue
+++ src/views/place/PlaceDetail.vue
@@ -1,27 +1,66 @@
 <template>
     <v-container fluid>
-        <v-layout justify-center>
-            <v-flex m8>
-                <v-card>
-                    <v-card-title primary-title>
-                        <h3>{{name}}</h3>
-                    </v-card-title>
-                    <v-card-text>
-                        <p>Address: {{address}}</p>
-                        <p>Phone: {{phone}}</p>
-                        <map-with-marker></map-with-marker>
-                    </v-card-text>
-                </v-card>
-            </v-flex>
+        <v-layout>
+            <div v-for="gym in gyms">
+                <v-flex xs12 sm6 offset-sm3>
+                    <v-card>
+                        <v-img
+                        src="https://cdn.vuetifyjs.com/images/lists/ali.png"
+                        height="300px"
+                        >
+                            <v-layout
+                                column
+                                fill-height
+                            >
+                                <v-card-title>
+                                
+
+                                <v-spacer></v-spacer>
+
+                                </v-card-title>
+
+                                <v-spacer></v-spacer>
+
+                                <v-card-title class="white--text pl-5 pt-5">
+                                    <div class="display-1 pl-5 pt-5">{{gym.name}}</div>
+                                </v-card-title>
+                            </v-layout>
+                        </v-img>
+
+                        <v-list two-line>
+                        <v-list-tile @click="">
+                            <v-list-tile-content>
+                                <v-list-tile-title>+{{gym.phone}}</v-list-tile-title>
+                                <v-list-tile-sub-title>Phone</v-list-tile-sub-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+
+                        <v-list-tile @click="">
+                            <v-list-tile-content>
+                                <v-list-tile-title>{{gym.address}}</v-list-tile-title>
+                                <v-list-tile-sub-title>Address</v-list-tile-sub-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                        </v-list>
+                    </v-card>
+                </v-flex>
+            </div>
+            
         </v-layout>
     </v-container>
 </template>
 
 <script>
+    import {mapState} from 'vuex'
     import MapWithMarker from "@/components/map/MapWithMarker";
     export default {
         name: "PlaceDetail",
         components: {MapWithMarker},
+        sportCenter: {
+          name: '',
+          address: '',
+          phone: ''  
+        },
         props: {
             name:{
                 default: 'Test'
@@ -35,6 +74,17 @@
         },
         component:{
             MapWithMarker: require('../../components/map/MapWithMarker')
+        },
+        created(){
+            console.log(this.$route.params.id)
+        },
+        computed: {
+            ...mapState('search', {
+                gym: state => state.gym
+            }),
+            gyms(){
+                return this.gym
+            }
         }
     }
 </script>
